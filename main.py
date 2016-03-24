9687070624139b702d6e92c7198233b1bf9f57e6
songlin <zhang@qq.com>
2016-03-25 00:12:42 +0800

    -

--- main.py
+++ main.py
@@ -19,33 +19,58 @@ class MyFrame(wx.Frame):
 		self.textFields = {}
 
 		#面板
-		panel = wx.Panel(self)
-		panel.SetBackgroundColour('White')
+		self.panel = wx.Panel(self)
+		self.panel.SetBackgroundColour('White')
 
 		#按钮
-		button = wx.Button(panel, label = u'对话...', size = (100, 30), pos = (20, 20))
+		button = wx.Button(self.panel, label = u'对话...', size = (100, 30), pos = (20, 20))
 		self.Bind(wx.EVT_BUTTON, self.OnButton, button)
 
 		#状态栏、工具栏
-		self.CreateStatusBar()
+		self.statusBar = self.CreateStatusBar()
 		self.toolBar = self.CreateToolBar()
 
+		#设置状态栏
+		self.statusBar.SetFieldsCount(3)
+		self.statusBar.SetStatusWidths([-1, -2, -3])
+
 		#菜单
-		self.menuBar = wx.MenuBar()
-		menu1 = wx.Menu()
-		textMenuItem = menu1.Append(id = wx.ID_ANY, text = u'文本', help = u'输入文本')
-		colorMenuItem = menu1.Append(id = wx.ID_ANY, text = u'选择颜色', help = u'请从列表中选择颜色')
+		menuBar = wx.MenuBar()
+		self.SetMenuBar(menuBar)
 
-		self.menuBar.Append(menu1, title = u'菜单')
-		self.SetMenuBar(self.menuBar)
+		menuFile = wx.Menu()
+		menuTool = wx.Menu()
+		menuEdit = wx.Menu()
+		menuBar.Append(menuFile, title = u'文件')
+		menuBar.Append(menuTool, title = u'编辑')
+		menuBar.Append(menuEdit, title = u'工具')
 
-		self.textFields['first_name'] = wx.TextCtrl(panel, id = wx.ID_ANY)
-		self.textFields['last_name']  = wx.TextCtrl(panel, id = wx.ID_ANY,pos = (200, 0))
+		menuColor = wx.Menu()
+		menuColor.AppendCheckItem(id = wx.ID_ANY, text = u'红色')
+		menuColor.AppendCheckItem(id = wx.ID_ANY, text = u'蓝色')
+		menuColor.AppendCheckItem(id = wx.ID_ANY, text = u'黄色')
+
+		menuCommonColor = wx.Menu()
+		menuCommonColor.Append(id = wx.ID_ANY, text = u'黑色', kind = wx.ITEM_CHECK)
+		menuCommonColor.Append(id = wx.ID_ANY, text = u'白色', kind = wx.ITEM_CHECK)
+		menuCommonColor.Append(id = wx.ID_ANY, text = u'红色', kind = wx.ITEM_CHECK)
+
+		menuColor.AppendMenu(id = wx.ID_ANY, text = u'常用色', submenu = menuCommonColor)
+
+		menuFormat = wx.Menu()
+		menuFormat.AppendRadioItem(id = wx.ID_ANY, text = 'txt')
+		menuFormat.AppendRadioItem(id = wx.ID_ANY, text = 'jpg')
+		menuFormat.AppendRadioItem(id = wx.ID_ANY, text = 'gif')
+
+		menuFile.AppendMenu(id = wx.ID_ANY, text = u'颜色', submenu = menuColor)
+		menuFile.AppendMenu(id = wx.ID_ANY, text= u'格式', submenu = menuFormat)
+
+		self.textFields['first_name'] = wx.TextCtrl(self.panel, id = wx.ID_ANY)
+		self.textFields['last_name']  = wx.TextCtrl(self.panel, id = wx.ID_ANY,pos = (200, 0))
 
 		#事件
-		self.Bind(wx.EVT_MENU, self.OnText, textMenuItem)
-		self.Bind(wx.EVT_MENU, self.OnChoice, colorMenuItem)
-		self.Bind(wx.EVT_LEFT_DCLICK, self.DoubleClick, panel)
+		self.Bind(wx.EVT_LEFT_DCLICK, self.DoubleClick, self.panel)
+		self.panel.Bind(wx.EVT_MOTION, self.OnMotion)
 
 		self.nameModel = namemodel.NameModel('', '')
 		self.nameModel.addListener(self.OnUpdate)
@@ -56,6 +81,10 @@ class MyFrame(wx.Frame):
 
 	def DoubleClick(self, e):
 		print u'双击'
+
+	def OnMotion(self, e):
+		self.statusBar.SetStatusText(str(e.GetPosition()), 2)
+		e.Skip()
 
 	def OnClick(self, e):
 		dlg = wx.MessageDialog(None, message = u'确定要执行以下操作吗？', caption = u'删除提示', style = wx.YES|wx.NO)
